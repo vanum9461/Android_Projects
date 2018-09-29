bb5d7fba963aafb066fa116de641a31f8d541589
vanum9461 <vanum9461@gmail.com>
2018-09-29 12:06:25 +0530

    youtubeapi

---
+++ ytube/app/src/main/java/com/example/hp/ytube/alpha.java
@@ -0,0 +1,12 @@
+package com.example.hp.ytube;
+
+public class alpha {
+    public alpha(){
+
+    }
+    private static final String API_KEY="AIzaSyB05kF-57G90dI6bMq31Ih6Rnsn1FUGp4w";
+    public static String getApiKey()
+    {
+        return API_KEY;
+    }
+}
